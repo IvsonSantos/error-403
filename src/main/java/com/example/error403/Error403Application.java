70fd06cc50a433c252e461f3b8f014b9d834e961
ivsonsantos <ivson.java@gmail.com>
2023-10-13 12:24:59 +0200

    Projeto criado

---
+++ src/main/java/com/example/error403/Error403Application.java
@@ -0,0 +1,13 @@
+package com.example.error403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Error403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Error403Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/error403/Error403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.error403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Error403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
