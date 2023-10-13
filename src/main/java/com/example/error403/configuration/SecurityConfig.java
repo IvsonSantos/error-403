62b1217f9f88f3282614a517d17155c19be1ecc8
ivsonsantos <ivson.java@gmail.com>
2023-10-13 12:44:52 +0200

    disabling CSRF protection

--- src/main/java/com/example/error403/configuration/SecurityConfig.java
+++ src/main/java/com/example/error403/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.error403.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -32,7 +33,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
-                .permitAll());
+                .permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
